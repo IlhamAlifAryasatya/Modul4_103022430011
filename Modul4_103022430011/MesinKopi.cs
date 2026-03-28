f4b8288299e34c9764f7a25c180125504218320c
Ilham <ialif9693@gmail.com>
2026-03-28 09:08:25 +0700

    menambahkan class MesinKopi

---
+++ Modul4_103022430011/MesinKopi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022430011
+{
+    public class MesinKopi
+    {
+        public enum State { OFF, STANDBY, BREWING, MAINTENANCE }
+        public State currentState;
+
+        public MesinKopi() 
+        {
+            currentState = State.OFF;
+        }
+
+        public void Transisi(string trigger)
+        {
+            State oldState = currentState;
+            bool valid = true;
+
+            switch (currentState)
+            {
+                case State.OFF:
+                    if (trigger == "POWER_ON") currentState = State.STANDBY;
+                    else valid = false;
+                    break;
+                case State.STANDBY:
+                    if (trigger == "POWER_OFF") currentState = State.OFF;
+                    else if (trigger == "START_BREW") currentState = State.BREWING;
+                    else if (trigger == "START_MAINTENANCE") currentState = State.MAINTENANCE;
+                    else valid = false;
+                    break;
+                case State.BREWING:
+                    if (trigger == "FINISH_BREW") currentState = State.STANDBY;
+                    else valid = false;
+                    break;
+                case State.MAINTENANCE:
+                    if (trigger == "FINISH_MAINTENANCE") currentState = State.STANDBY;
+                    else valid = false;
+                    break;
+            }
+            if (valid)
+            {
+                Console.WriteLine($"MESIN {oldState} berubah menjadi {currentState}");
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+    }
+}
--- Modul4_103022430011/Program.cs
+++ Modul4_103022430011/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Modul4_103022430011;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePaket paket = new KodePaket();
+        Console.WriteLine("--- Output Table Driven ---");
+        Console.WriteLine("Paket 'Basic': " + paket.getKodePaket("Basic"));
+        Console.WriteLine("Paket 'Standard': " + paket.getKodePaket("Standard"));
+        Console.WriteLine("Paket 'Premium': " + paket.getKodePaket("Premium"));
+        Console.WriteLine("Paket 'Unlimited': " + paket.getKodePaket("Unlimited"));
+        Console.WriteLine("Paket 'Gaming': " + paket.getKodePaket("Gaming"));
+        Console.WriteLine("Paket 'Streaming': " + paket.getKodePaket("Streaming"));
+        Console.WriteLine("Paket 'Family': " + paket.getKodePaket("Family"));
+        Console.WriteLine("Paket 'Business': " + paket.getKodePaket("Business"));
+        Console.WriteLine("Paket 'Student': " + paket.getKodePaket("Student"));
+        Console.WriteLine("Paket 'Traveler': " + paket.getKodePaket("Traveler"));
+        Console.WriteLine();
+
+        MesinKopi mesin = new MesinKopi();
+        Console.WriteLine("--- Output State Based ---");
+        mesin.Transisi("POWER_ON");
+        mesin.Transisi("START_BREW");
+        mesin.Transisi("FINISH_BREW");
+        mesin.Transisi("START_MAINTENANCE");
+        mesin.Transisi("FINISH_MAINTENANCE");
+        mesin.Transisi("POWER_OFF");
+        mesin.Transisi("START_BREW");
+    }
+}
